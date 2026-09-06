37526045a199eb879e1705786a4ba32ae6837d2a
poprox <hudec.matyas.2006@gmail.com>
2026-09-06 14:54:49 +0200

    Add missing java lib for linux

--- server/core/build.gradle.kts
+++ server/core/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
 
 	implementation("dev.onvoid.webrtc:webrtc-java:0.14.0")
 	implementation("dev.onvoid.webrtc:webrtc-java:0.14.0:windows-x86_64")
+	implementation("dev.onvoid.webrtc:webrtc-java:0.14.0:linux-x86_64")
 	implementation("org.jmdns:jmdns:3.6.3")
 	implementation("io.ktor:ktor-client-core:3.4.1")
 	implementation("io.ktor:ktor-client-cio:3.4.1")
